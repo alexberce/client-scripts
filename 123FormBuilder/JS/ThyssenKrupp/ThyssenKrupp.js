1fad18dc645eb5bf6358df803977b618a51f7fe3
Alexandru Berce <alex@123formbuilder.com>
2019-02-25 14:23:41 +0200

    Update ThyssenKrupp.js
    Fixed script

--- 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
+++ 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   setTimeout(() => {
     for(let i=0; i < 10; i++)
-      jQuery("[data-role="add-group-button"]").click();
+      jQuery("[data-role='add-group-button']").click();
   }, 10);
 });
