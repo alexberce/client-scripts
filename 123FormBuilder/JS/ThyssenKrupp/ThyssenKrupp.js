89e78024608a5f648265d3e5a8f7fb32437cc705
Alexandru Berce <alex@123formbuilder.com>
2019-04-12 14:03:38 +0300

    Update ThyssenKrupp.js

--- 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
+++ 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
@@ -24,7 +24,7 @@
       for(let i=0; i < numberOfRowsToAdd; i++){
         setTimeout(() => {
           jQuery("[data-role='add-group-button']").click();
-        }, 1);
+        }, 5);
       }
 
       jQuery('[data-id="' + topHTMLBlockFieldId + '"] table').eq(0).attr('cellspacing', '0px');
