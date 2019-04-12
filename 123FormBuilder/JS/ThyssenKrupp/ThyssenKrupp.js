026d924e1ad1dda05b2ea2c3fa41a0865b18a2b6
Alexandru Berce <alex@123formbuilder.com>
2019-04-12 15:10:14 +0300

    Update ThyssenKrupp.js

--- 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
+++ 123FormBuilder/JS/ThyssenKrupp/ThyssenKrupp.js
@@ -27,7 +27,7 @@
       //   }, i * 25);
       // }
 
-      loader.getDOMAbstractionLayer().setControlValueById(String(hoursFieldId), "", ["","","","","","",""]);
+      loader.getDOMAbstractionLayer().setControlValueById(String(hoursFieldId), "", new Array(numberOfRowsToAdd).fill(''));
 
       jQuery('[data-id="' + topHTMLBlockFieldId + '"] table').eq(0).attr('cellspacing', '0px');
 
